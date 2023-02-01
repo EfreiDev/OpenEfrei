15927c0941ea1cd40bc3d514974412eea847d8e6
Fyz <35635613+FyzRDC@users.noreply.github.com>
2023-02-01 23:35:26 +0100

    Initial commit

---
+++ src/main/java/dev/efrei/openefrei/OpenefreiApplication.java
@@ -0,0 +1,14 @@
+package dev.efrei.openefrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenefreiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenefreiApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/dev/efrei/openefrei/OpenefreiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.efrei.openefrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenefreiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
