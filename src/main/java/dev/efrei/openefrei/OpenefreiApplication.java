244955d5c8ec7f2035783f62ce6d5229704afe07
Fyz <35635613+FyzRDC@users.noreply.github.com>
2023-02-10 17:23:34 +0100

    Removed tests

--- src/main/java/dev/efrei/openefrei/OpenefreiApplication.java
+++ src/main/java/dev/efrei/openefrei/OpenefreiApplication.java
@@ -3,24 +3,11 @@ package dev.efrei.openefrei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.gson.Gson;
-
-import dev.efrei.openefrei.managers.users.UserEntity;
-
 @SpringBootApplication
 public class OpenefreiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenefreiApplication.class, args);
-		
-		UserEntity test = new UserEntity();
-		test.setAdmin(false);
-		test.setEfreiID("efreIDlol");
-		test.setEmail("salut@yo.fr");
-		test.setFirstName("John");
-		test.setLastName("Doe");
-		test.setPassword("test");
-		System.out.println(new Gson().toJson(test));
 	}
 
 }
